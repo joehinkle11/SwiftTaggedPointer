47c1a2c33a07f51c2025ec7a7117276926364846
Joe Hinkle <joehinkle11@gmail.com>
2023-10-26 01:03:20 -0700

    initial commit

---
+++ Sources/SwiftTaggedPointer/SwiftTaggedPointer.swift
@@ -0,0 +1,288 @@
+//
+//  SwiftTaggedPointer.swift
+//
+//
+//  Created by Joseph Hinkle on 10/25/23.
+//
+// This file can be used by itself of you can use the Swift Package.
+//
+// We assume that:
+//  1. The provided pointers are 8 byte aligned
+//  2. The program is running in user space (not kernel space)
+//  3. The program is running on a 64 bit system
+//  4. The program is running on a little endian system
+//  5. The OS is not using the top 16 bits of pointers (which is true for all current OSes)
+//
+// The result is the following layout:
+//  1. The top 17 bits are always 0
+//  2. The next 44 bits are all the significant bits of the pointer
+//  3. The bottom 3 bits are the tag
+//
+// The layout visualized:
+//                 Raw storage:  |-----------------------------64 bits----------------------------|
+//             Pointer anatomy:  |--16 bits--|------1 bit-----|---- 44 bits ---|------3 bits------|
+//                               |unused bits|is kernel space?|the pointer guts|alignment artifact|
+//                               |   all 0s  |       0        |      ???       |      all 0s      |
+//                 Raw storage:  |-----------------------------64 bits----------------------------|
+//  Simplified Pointer anatomy:  |-----17 bits-----|--------------44 bits--------------|--3 bits--|
+//                               |     all 0s      |    significant bits of pointer    |  all 0s  |
+//                               |----------------------------------------------------------------|
+//
+// Some explanation:
+//  The top 16 bits are always 0 because the OS assumes that memory never goes above what a 48 bit pointer
+//  can address (48 bits gives you 256 TB of memory). The kernel space flag is 1 when the pointer is in kernel
+//  space and 0 when the pointer is in user space. We assume that the program is running in user space. The
+//  bottom 3 bits are always 0 because the pointers are 8 byte aligned, i.e. the first address would be 0x0,
+//  then 0x8, then 0x10, etc. If you convert the pointer hex value to binary, you will see that the bottom 3
+//  bits are always 0.
+//
+
+/// A pointer that stores a 3 bit tag and a 17 bit integer within a pointer.
+/// You can either use the tag as a 3 bit integer (represented as UInt8 `tagAsUInt8`) or as 3 individual bits (`bitTag0`, `bitTag1`, `bitTag2`).
+/// You can either use the data as a 17 bit signed integer (`data17`) or as a 16 bit unsigned integer (`dataUInt16`).
+/// You can also directly access the sign bit of the data (`signBit`).
+/// ```
+/// Memory layout visualized:
+///            Raw storage:  |-----------------------------64 bits----------------------------|
+///  TaggedPointer anatomy:  |-------17 bits-------|------44 bits-------|--------3 bits-------|
+///                          | custom 17 bit data  |    pointer data    |  custom 3 bit tag   |
+///                          |                     |    `getPointer`    |                     | 
+///                          |                     |    `setPointer`    |                     |
+///                          |----------------------------------------------------------------|
+///
+///                          |-----------------------------64 bits----------------------------|
+///                          |--------61 bits-------|-----------------3 bits------------------|
+///                          |                      |               `tagUInt3`                |
+///                          |                      |----1 bit----|----1 bit----|----1 bit----|
+///                          |                      |   `bitTag2` |  `bitTag1`  |  `bitTag0`  |
+///                          |----------------------------------------------------------------|
+///
+///                          |-----------------------------64 bits----------------------------|
+///                          |------------17 bits----------|--------------47 bits-------------|
+///                          |          `dataInt17`        |                                  |
+///                          |-----16 bits-----|---1 bit---|                                  |
+///                          |   magnitude     | `signBit` |                                  |
+///                          |----------------------------------------------------------------|
+///
+///                          |-----------------------------64 bits----------------------------|
+///                          |-----16 bits-----|---------------------48 bits------------------|
+///                          |   `dataUInt16`  |                                              |
+///                          |   `dataInt16`   |                                              |
+///                          |----------------------------------------------------------------|
+/// ```
+public struct TaggedPointer<P : _Pointer>: Equatable {
+    
+    #if !DEBUG
+    @usableFromInline
+    @_alwaysEmitIntoClient
+    #endif
+    internal var _storage: Int
+    
+    #if !DEBUG
+    @inline(__always)
+    @inlinable
+    @_alwaysEmitIntoClient
+    #endif
+    public init(_ pointer: P?) {
+        self._storage = 0
+        self.setPointer(pointer)
+    }
+    
+    #if !DEBUG
+    @inline(__always)
+    @inlinable
+    @_alwaysEmitIntoClient
+    #endif
+    public func getPointer(_ type: P.Type = P.self) -> P? {
+        assert(MemoryLayout<P>.size == MemoryLayout<UInt64>.size)
+        // Make pointer canonical by setting top 17 bits to 0 and bottom 3 bits to 0
+        return P(bitPattern: _storage & canonicalPointerGutsMask)
+    }
+
+    #if !DEBUG
+    @inline(__always)
+    @inlinable
+    @_alwaysEmitIntoClient
+    #endif
+    public mutating func setPointer(_ pointer: P?) {
+        assert(MemoryLayout<P>.size == MemoryLayout<UInt64>.size)
+        assert(Int(bitPattern: pointer) & canonicalPointerGutsMaskNegated == 0)
+        // Clear pointer guts
+        _storage &= canonicalPointerGutsMaskNegated
+        // Set pointer guts
+        _storage |= Int(bitPattern: pointer)
+    }
+
+    /// The 3 bit tag
+    #if !DEBUG
+    @inline(__always)
+    @inlinable
+    @_alwaysEmitIntoClient
+    #endif
+    public var tagUInt3: UInt8 {
+        get {
+            return UInt8(_storage & bottom3BitsMask)
+        }
+        set {
+            assert(Int(newValue) & bottom3BitsMaskNegated == 0)
+            // Clear tag
+            _storage &= bottom3BitsMaskNegated
+            // Set tag
+            _storage |= Int(newValue)
+        }
+    }
+
+    /// The first bottom bit of the tag
+    #if !DEBUG
+    @inline(__always)
+    @inlinable
+    @_alwaysEmitIntoClient
+    #endif
+    public var bitTag0: Bool {
+        get {
+            return _storage & 0b1 != 0
+        }
+        set {
+            if newValue {
+                _storage |= 0b1
+            } else {
+                _storage &= ~0b1
+            }
+        }
+    }
+
+    /// The second to bottom bit of the tag
+    #if !DEBUG
+    @inline(__always)
+    @inlinable
+    @_alwaysEmitIntoClient
+    #endif
+    public var bitTag1: Bool {
+        get {
+            return _storage & 0b10 != 0
+        }
+        set {
+            if newValue {
+                _storage |= 0b10
+            } else {
+                _storage &= ~0b10
+            }
+        }
+    }
+
+    /// The third bottom bit of the tag
+    #if !DEBUG
+    @inline(__always)
+    @inlinable
+    @_alwaysEmitIntoClient
+    #endif
+    public var bitTag2: Bool {
+        get {
+            return _storage & 0b100 != 0
+        }
+        set {
+            if newValue {
+                _storage |= 0b100
+            } else {
+                _storage &= ~0b100
+            }
+        }
+    }
+
+    /// The 16 bit unsigned integer
+    #if !DEBUG
+    @inline(__always)
+    @inlinable
+    @_alwaysEmitIntoClient
+    #endif
+    public var dataUInt16: UInt16 {
+        get {
+            return UInt16(bitPattern: Int16(_storage >> 48))
+        }
+        set {
+            assert(Int(bitPattern: UInt(newValue)) << 48 & top16BitsMaskNegated == 0)
+            // Clear data
+            _storage &= top16BitsMaskNegated
+            // Set data
+            _storage |= Int(bitPattern: UInt(newValue)) << 48
+        }
+    }
+
+    /// The sign bit (which corresponds to `dataInt17`).
+    #if !DEBUG
+    @inline(__always)
+    @inlinable
+    @_alwaysEmitIntoClient
+    #endif
+    public var signBit: Bool {
+        get {
+            return _storage & signBitMask != 0
+        }
+        set {
+            if newValue {
+                _storage |= signBitMask
+            } else {
+                _storage &= signBitMaskNegated
+            }
+        }
+    }
+
+    /// The 17 bit signed integer. The magnitude of this integer is stored in `dataUInt16`.
+    #if !DEBUG
+    @inline(__always)
+    @inlinable
+    @_alwaysEmitIntoClient
+    #endif
+    public var dataInt17: Int32 {
+        get {
+            return self.signBit ? -Int32(dataUInt16) - 1 : Int32(dataUInt16)
+        }
+        set {
+            assert(newValue >= -65536 && newValue <= 65535)
+            // Clear data
+            _storage &= top17BitsMaskNegated
+            if newValue < 0 {
+                assert(Int(bitPattern: UInt(-newValue - 1)) << 48 & top16BitsMaskNegated == 0)
+                // Set sign bit
+                _storage |= signBitMask
+                // Set magnitude
+                _storage |= Int(bitPattern: UInt(-newValue - 1)) << 48
+            } else {
+                assert(Int(bitPattern: UInt(newValue)) << 48 & top16BitsMaskNegated == 0)
+                // Set magnitude
+                _storage |= Int(bitPattern: UInt(newValue)) << 48
+            }
+        }
+    }
+}
+
+// Constants
+@_alwaysEmitIntoClient
+private let canonicalPointerGutsMask: Int = Int(bitPattern: 0b0000000000000000011111111111111111111111111111111111111111111000 as UInt)
+@_alwaysEmitIntoClient
+private let canonicalPointerGutsMaskNegated: Int = ~canonicalPointerGutsMask
+@_alwaysEmitIntoClient
+private let bottom3BitsMask: Int = Int(bitPattern: 0b111 as UInt)
+@_alwaysEmitIntoClient
+private let bottom3BitsMaskNegated: Int = ~bottom3BitsMask
+@_alwaysEmitIntoClient
+private let top17BitsMask: Int = Int(bitPattern: 0b1111111111111111100000000000000000000000000000000000000000000000 as UInt)
+@_alwaysEmitIntoClient
+private let top17BitsMaskNegated: Int = ~top17BitsMask
+@_alwaysEmitIntoClient
+private let top16BitsMask: Int = Int(bitPattern: 0b1111111111111111000000000000000000000000000000000000000000000000 as UInt)
+@_alwaysEmitIntoClient
+private let top16BitsMaskNegated: Int = ~top16BitsMask
+@_alwaysEmitIntoClient
+private let signBitMask: Int = Int(bitPattern: 0b100000000000000000000000000000000000000000000000 as UInt)
+@_alwaysEmitIntoClient
+private let signBitMaskNegated: Int = ~signBitMask
+
+
+// Compile time asserts
+#if _endian(big)
+#error("This code assumes little endian")
+#endif
+
+
+
---
+++ Tests/SwiftTaggedPointerTests/SwiftTaggedPointerTests.swift
@@ -0,0 +1,91 @@
+import XCTest
+@testable import SwiftTaggedPointer
+
+final class SwiftTaggedPointerTests: XCTestCase {
+    func testExample() throws {
+        let x = 5
+        withUnsafePointer(to: x) { p in
+            let y = 5
+            withUnsafePointer(to: y) { p2 in
+                var tp = TaggedPointer(p)
+                let tp2 = TaggedPointer(p2)
+                XCTAssertNotEqual(tp, tp2)
+                XCTAssertNotEqual(p, p2)
+                XCTAssertNotEqual(tp.getPointer(), tp2.getPointer())
+                XCTAssertEqual(p, tp.getPointer())
+                XCTAssertEqual(tp.tagUInt3, 0)
+                XCTAssertEqual(tp.bitTag0, false)
+                tp.bitTag0 = true
+                XCTAssertEqual(tp.tagUInt3, 1)
+                XCTAssertEqual(tp.bitTag0, true)
+                XCTAssertEqual(p, tp.getPointer())
+                XCTAssertEqual(tp.bitTag0, true)
+                tp.bitTag0 = false
+                XCTAssertEqual(tp.tagUInt3, 0)
+                XCTAssertEqual(tp.bitTag0, false)
+                tp.bitTag1 = true
+                XCTAssertEqual(tp.tagUInt3, 2)
+                XCTAssertEqual(tp.bitTag1, true)
+                XCTAssertEqual(tp.bitTag0, false)
+                XCTAssertEqual(tp.bitTag2, false)
+                tp.bitTag2 = true
+                XCTAssertEqual(tp.tagUInt3, 6)
+                XCTAssertEqual(tp.bitTag2, true)
+                tp.tagUInt3 = 1
+                XCTAssertEqual(tp.bitTag0, true)
+                XCTAssertEqual(tp.bitTag1, false)
+                XCTAssertEqual(tp.bitTag2, false)
+                XCTAssertEqual(tp.dataUInt16, 0)
+                XCTAssertEqual(tp.signBit, false)
+                XCTAssertEqual(p, tp.getPointer())
+                tp.signBit = true
+                XCTAssertEqual(tp.dataUInt16, 0)
+                XCTAssertEqual(tp.signBit, true)
+                XCTAssertEqual(p, tp.getPointer())
+                tp.dataUInt16 = .max
+                XCTAssertEqual(tp.dataUInt16, .max)
+                XCTAssertEqual(tp.signBit, true)
+                XCTAssertEqual(p, tp.getPointer())
+                tp.dataUInt16 = 0
+                XCTAssertEqual(tp.dataUInt16, 0)
+                XCTAssertEqual(tp.signBit, true)
+                XCTAssertEqual(p, tp.getPointer())
+                tp.dataInt17 = 37
+                XCTAssertEqual(tp.dataInt17, 37)
+                XCTAssertEqual(tp.dataUInt16, 37)
+                XCTAssertEqual(tp.signBit, false)
+                tp.signBit = true
+                XCTAssertEqual(tp.dataInt17, -38)
+                XCTAssertEqual(tp.dataUInt16, 37)
+                XCTAssertEqual(tp.signBit, true)
+                tp.dataInt17 = 0
+                XCTAssertEqual(tp.dataInt17, 0)
+                XCTAssertEqual(tp.dataUInt16, 0)
+                XCTAssertEqual(tp.signBit, false)
+                tp.signBit = true
+                XCTAssertEqual(tp.dataInt17, -1)
+                XCTAssertEqual(tp.dataUInt16, 0)
+                XCTAssertEqual(tp.signBit, true)
+                tp.dataInt17 = -5
+                XCTAssertEqual(tp.dataInt17, -5)
+                XCTAssertEqual(tp.dataUInt16, 4)
+                XCTAssertEqual(tp.signBit, true)
+                tp.signBit = false
+                XCTAssertEqual(tp.dataInt17, 4)
+                XCTAssertEqual(tp.dataUInt16, 4)
+                XCTAssertEqual(tp.signBit, false)
+                for uint17: Int32 in -65536...65535 {
+                    tp.dataInt17 = uint17
+                    XCTAssertEqual(tp.dataInt17, uint17)
+                    XCTAssertEqual(tp.dataUInt16, uint17 < 0 ? UInt16(-(uint17 + 1)) : UInt16(uint17))
+                    XCTAssertEqual(tp.signBit, uint17 < 0)
+                    XCTAssertEqual(p, tp.getPointer())
+                    XCTAssertEqual(tp.bitTag0, true)
+                    XCTAssertEqual(tp.bitTag1, false)
+                    XCTAssertEqual(tp.bitTag2, false)
+                    XCTAssertEqual(tp.tagUInt3, 2)
+                }
+            }
+        }
+    }
+}
